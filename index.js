d7630fcc9581bb7ba223f06433fc1979a96b89d3
Davitbakradze <davitibaqradze98@gmail.com>
2024-02-13 23:57:37 +0400

    created first slider example

---
+++ index.js
@@ -0,0 +1,28 @@
+/** @format */
+
+let index = 0;
+let transitionDelay = 5000;
+
+let slideContainer = document.querySelector(".slideshow");
+
+let slides = slideContainer.querySelectorAll(".slide");
+
+for (let slide of slides) {
+  slide.style.transition = `all ${transitionDelay / 1500}s linear`;
+}
+
+showSlide(index);
+
+function showSlide(slideNumber) {
+  slides.forEach((slide, i) => {
+    slide.style.display = i == slideNumber ? "block" : "none";
+  });
+
+  index++;
+
+  if (index >= slides.length) {
+    index = 0;
+  }
+}
+
+setInterval(() => showSlide(index), transitionDelay);
